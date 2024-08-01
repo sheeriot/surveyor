f9d2118f728b794c5565da9d4198fbe79080e51a
Kris Thompson <kris@austinvoicedata.com>
2024-08-01 14:55:34 +0000

    cleanup mess

--- surveyor/webapp/device/templatetags/dataframe_template_tags.py
+++ surveyor/webapp/device/templatetags/dataframe_template_tags.py
@@ -51,7 +51,7 @@ def dataframe_to_htmltablerows_withlinks(df):
 
 # def convert_data_frame_to_html_table_rows(df):
 @register.simple_tag
-def dataframe_to_htmltablerows_withlinks2(df, source_id=None, report_group=None,
+def dataframe_to_htmltablerows_withlinks2(df, source_id=None,
                                           meas=None, start_mark=None, end_mark=None):
     html = ""
     for index, row in df.iterrows():
